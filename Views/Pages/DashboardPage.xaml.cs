6ce6332007ed90589787d7ba75b68b2a76c489d0
Mijail Todorovich <mijailtodorovich+git@gmail.com>
2024-03-21 16:13:37 -0300

    initial

---
+++ Views/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using Wpf.Ui.Controls;
+using WPFUINumberBoxBugDemo.ViewModels.Pages;
+
+namespace WPFUINumberBoxBugDemo.Views.Pages
+{
+    public partial class DashboardPage : INavigableView<DashboardViewModel>
+    {
+        public DashboardViewModel ViewModel { get; }
+
+        public DashboardPage(DashboardViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TEST.Value = 20;
+        }
+    }
+}
